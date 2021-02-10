614896b1970954a2b9407208370377dd66a115e5
beardotheweirdo1 <45885605+beardotheweirdo1@users.noreply.github.com>
2021-02-10 14:26:00 +0000

    growth rates changes

--- growth rates.py
+++ growth rates.py
@@ -7,8 +7,11 @@ def inverse_x(x, a, b):
     return (a*np.log(x))+b
 
 
-days_since_year = [1, 22, 35, 56, 63, 70, 105, 119, 147, 161, 175, 189]
-growth_rates = [0.0520, 0.0506, 0.0376, 0.0296, 0.0363, 0.0301, 0.0250, 0.0258, 0.0221, 0.0215, 0.0195, 0.0202]
+days_since_year1 = [1, 22, 35, 56, 63, 70, 105, 119, 147, 161, 175, 189]
+days_since_year2 = [x+365 for x in days_since_year1]
+days_since_year = days_since_year1 + days_since_year2
+print(days_since_year)
+growth_rates = [0.0520, 0.0506, 0.0376, 0.0296, 0.0363, 0.0301, 0.0250, 0.0258, 0.0221, 0.0215, 0.0195, 0.0202,0.0520, 0.0506, 0.0376, 0.0296, 0.0363, 0.0301, 0.0250, 0.0258, 0.0221, 0.0215, 0.0195, 0.0202]
 
 
 curve_fit = opt.curve_fit(inverse_x, days_since_year, growth_rates)

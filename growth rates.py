e101f1a7c6a1c93031ec236f79354d86ec946db2
mcrowls <73698289+mcrowls@users.noreply.github.com>
2021-02-11 13:52:06 +0000

    Managed to fit the sine curve whoop. Code and figure added

--- growth rates.py
+++ growth rates.py
@@ -1,23 +1,28 @@
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
 import numpy as np
+import math
 
-
-def inverse_x(x, a, b):
-    return (a*np.log(x))+b
+def cos_x(x, a, b, d):
+    return a*np.cos((b*x))+d
 
 
 days_since_year = [1, 22, 35, 56, 63, 70, 105, 119, 147, 161, 175, 189]
 growth_rates = [0.0520, 0.0506, 0.0376, 0.0296, 0.0363, 0.0301, 0.0250, 0.0258, 0.0221, 0.0215, 0.0195, 0.0202]
+for j in range(1, 4):
+    for i in range(np.size(days_since_year)):
+        days_since_year.append(days_since_year[i]- j*365)
+        growth_rates.append(growth_rates[i])
 
+amplitude = (np.max(growth_rates)-np.min(growth_rates))/2
+initials = [amplitude, 2*math.pi/365, 2*amplitude]
+print(initials)
+curve_fit = opt.curve_fit(cos_x, days_since_year, growth_rates, p0=initials)
+xs = np.linspace(np.min(days_since_year), np.max(days_since_year), 10000)
+ys = cos_x(xs, curve_fit[0][0], curve_fit[0][1], curve_fit[0][2])
 
-curve_fit = opt.curve_fit(inverse_x, days_since_year, growth_rates)
-print(curve_fit)
-xs = np.linspace(1, 200, 10000)
-ys = inverse_x(xs, curve_fit[0][0], curve_fit[0][1])
 plt.plot(xs, ys)
 plt.scatter(days_since_year, growth_rates)
 plt.xlabel('Days since the start of the year when weaning begins')
 plt.ylabel('Growth Rate for this group of pigs (a.u)')
 plt.show()
-

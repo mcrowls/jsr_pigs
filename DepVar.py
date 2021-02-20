ef6f543b6d7d551f887387b3bbcab542e62313e6
CharlieJWMarsh <charlesjwmarsh@gmail.com>
2021-02-20 17:45:24 +0000

    Fixed insemination to work with new functions and removed weight mean,sd and bf mean,sd

--- DepVar.py
+++ DepVar.py
@@ -5,14 +5,6 @@
 SowParityMean = (10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10)
 SowParitySD = (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
 
-# Variables used to calculate the weight of a new born piglet
-WeightBA_Mean = 10
-WeightBA_SD = 1
-
-# Variables used to calculate the back fat of a new born piglet
-BackFatBA_Mean = 2
-BackFatBA_SD = 0.2
-
 # Variable used to calculate the pregnancy period for each sow
 PregPeriodMean = 114.605
 PregPeriodSD = 1.572
--- Insemination.py
+++ Insemination.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 
 # File to update the sow and piglet dataset after an insemination event
 
@@ -18,42 +17,34 @@ def Int_Distribution(Sigma, Mu):
 
 # Function to determine the weight of a new born piglet
 # Takes:
-#   Sigma - The mean of the normal distribution
-#   Mu -  The standard deviation of the normal distribution
+#   t - Time in days
+#   growth_rate - the growth rate of the piglet
 # Returns:
-#   Value - The randomly chosen value
+#   y - the updated weight of the pig
 def CalculateWeight(t, growth_rate):
     y = 240 / (1 + 30 * np.exp(-growth_rate * t))
     return y
 
 
-def get_growth_rate_for_pig():
+# Function to determine the growth rate of a new born piglet
+# Takes:
+# Returns:
+#   GrowthRate - The growth rate of the pig
+def GenerateGrowthRate():
     mean = 0.018460179351268462
     var = 9.078204438994627e-08
-    return np.random.normal(mean, var, 1)
+    GrowthRate = np.random.normal(mean, var, 1)
+    return GrowthRate
 
 
 # Function to determine the depth of back fat of a new born piglet
 # Takes:
-#   Sigma - The mean of the normal distribution
-#   Mu -  The standard deviation of the normal distribution
+#   Weight - The current weight of the pig
 # Returns:
-#   Value - The randomly chosen value
+#   fat - The updated depth of fat
 def CalculateBackFat(weight):
     fat = 1.0364296522510674 + weight*0.08841889413839167
-    return Value
-
-
-# Function to determine the pregnancy period of a sow in days
-# Takes:
-#   Sigma - The mean of the normal distribution
-#   Mu -  The standard deviation of the normal distribution
-# Returns:
-#   Value - The randomly chosen integer
-def PregnancyPeriod(Sigma, Mu):
-    Value = np.random.normal(Sigma, Mu)
-    Value = int(round(Value))
-    return Value
+    return fat
 
 
 # Function to generate all the new data for each for each piglet a sow has birthed
@@ -64,37 +55,35 @@ def PregnancyPeriod(Sigma, Mu):
 #   F - The Farm the piglet is birthed onto
 #   SP_Mean - The list of mean piglets born alive depending on sow parity
 #   SP_SD - The list of standard deviations of piglets born alive depending on sow parity
-#   WeightMean - The mean weight of piglets born alive
-#   WeightSD - The standard deviation of weights of piglets born alive
-#   BF_Mean - The mean depth of back far of piglets born alive
-#   BF_SD - The standard deviation of depths of back far of piglets born alive
 #   PregMean - The mean length of pregnancy for sows
 #   PregSD - The standard deviation of pregnancy periods
 #   DF - The already existing dataset of piglets (If none a dummy is passed)
 #   DF_Set - A True/False variable saying whether there is already a dataset of piglets
 # Returns:
 #   DS - The dataset of piglets
-def GenPigletData(D, SN, SP, F, SP_Mean, SP_SD, WeightMean, WeightSD, BF_Mean, BF_SD, PregMean, PregSD, DF, DF_Set):
+def GenPigletData(D, SN, SP, F, SP_Mean, SP_SD, PregMean, PregSD, DF, DF_Set):
     # Calculate the number of piglets the sow has birthed
     BA = Int_Distribution(SP_Mean[SP], SP_SD[SP])
     # Initiate dataset
-    DS = np.zeros((BA, 10))
+    DS = np.zeros((BA, 11))
     # Generate piglet number
     if not DF_Set:
         DS[:, 0] = np.linspace(1, BA, BA)
     else:
         DS[:, 0] = np.linspace(1 + len(DF), len(DF) + BA, BA)
     for i in range(0, BA):
+        # Generate the growth rate for each piglet
+        DS[i, 10] = GenerateGrowthRate()
         # Generate weight for each piglet
-        DS[i, 1] = CalculateWeight(WeightMean, WeightSD)
+        DS[i, 1] = CalculateWeight(D, DS[i, 10])
         # Generate the back fat depth for each piglet
-        DS[i, 2] = CalculateBackFat(BF_Mean, BF_SD)
+        DS[i, 2] = CalculateBackFat(DS[i, 1])
     # Assign farm
     DS[:, 3] = F
     # Assign day inseminated
     DS[:, 4] = D
     # Generate the birthing date for the set of piglets
-    DS[:, 5] = PregnancyPeriod(PregMean, PregSD) + D
+    DS[:, 5] = Int_Distribution(PregMean, PregSD) + D
     # Assign Alive = True
     DS[:, 6] = 1
     # Assign sow number
@@ -111,10 +100,6 @@ def GenPigletData(D, SN, SP, F, SP_Mean, SP_SD, WeightMean, WeightSD, BF_Mean, B
 #   D - The day the simulation is on
 #   SP_Mean - The list of mean piglets born alive depending on sow parity
 #   SP_SD - The list of standard deviations of piglets born alive depending on sow parity
-#   WeightMean - The mean weight of piglets born alive
-#   WeightSD - The standard deviation of weights of piglets born alive
-#   BF_Mean - The mean depth of back far of piglets born alive
-#   BF_SD - The standard deviation of depths of back far of piglets born alive
 #   PregMean - The mean length of pregnancy for sows
 #   PregSD - The standard deviation of pregnancy periods
 #   SowDS - The sow dataset
@@ -122,17 +107,17 @@ def GenPigletData(D, SN, SP, F, SP_Mean, SP_SD, WeightMean, WeightSD, BF_Mean, B
 # Returns:
 #   DS - The dataset of piglets
 #   SetDS - A True/False variable saying whether there is already a dataset of piglets
-def Insemination(D, SP_Mean, SP_SD, WeightMean, WeightSD, BF_Mean, BF_SD, PregMean, PregSD, SowDS, SetDS,
+def Insemination(D, SP_Mean, SP_SD, PregMean, PregSD, SowDS, SetDS,
                  DS):
     # Iterates through sows adding more piglets to the piglet dataset by either creating a piglet dataset or
     # concatenating them to the end of the existing piglet dataset
     for i in range(0, len(SowDS)):
         if not SetDS:
-            DS = GenPigletData(D, int(SowDS[i, 0]), int(SowDS[i, 1]), int(SowDS[i, 2]), SP_Mean, SP_SD, WeightMean,
-                               WeightSD, BF_Mean, BF_SD, PregMean, PregSD, DS, SetDS)
+            DS = GenPigletData(D, int(SowDS[i, 0]), int(SowDS[i, 1]), int(SowDS[i, 2]), SP_Mean, SP_SD, PregMean,
+                               PregSD, DS, SetDS)
             SetDS = True
         else:
-            TempDS = GenPigletData(D, int(SowDS[i, 0]), int(SowDS[i, 1]), int(SowDS[i, 2]), SP_Mean, SP_SD, WeightMean,
-                                   WeightSD, BF_Mean, BF_SD, PregMean, PregSD, DS, SetDS)
+            TempDS = GenPigletData(D, int(SowDS[i, 0]), int(SowDS[i, 1]), int(SowDS[i, 2]), SP_Mean, SP_SD, PregMean,
+                                   PregSD, DS, SetDS)
             DS = np.concatenate((DS, TempDS))
     return DS, SetDS

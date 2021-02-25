dc7387ed4d5a30d766fa834ece5ab76ba4e942ae
CharlieJWMarsh <charlesjwmarsh@gmail.com>
2021-02-25 18:28:24 +0000

    Added new values for sow parity and born alive

--- DepVar.py
+++ DepVar.py
@@ -1,9 +1,5 @@
 # This file is for variables to do with calculations in the simulation
 
-# Variables to calculate the number of piglets born from each sow where the index corresponds to the sow parity
-SowParityMean = (9.89634522e-03 * (x ** 3)) - (2.80572294e-01 * (x ** 2)) + (1.75100075e+00 * x) + 1.30828185e+01
-SowParitySD = 3.6799672876782386
-
 # Variable used to calculate the pregnancy period for each sow
 PregPeriodMean = 114.605
 PregPeriodSD = 1.572

5bdd0e3b172b643e4cdeb01a2e60e5ecfaf2a93c
DozeyBot <DozeyIs@gmail.com>
2021-02-28 15:47:32 +0000

    number of sows per sow parity & farm found

--- InitVar.py
+++ InitVar.py
@@ -1,6 +1,10 @@
 # This file is for variables to do with initiating the simulation
 
 # The number of sows of sow parity corresponding to the index value of the list for each farm
-GenerateSP_EB = (3, 3, 3, 3, 3, 3, 3, 3, 3, 3)
-GenerateSP_HW = (3, 3, 3, 3, 3, 3, 3, 3, 3, 3)
-GenerateSP_SB = (3, 3, 3, 3, 3, 3, 3, 3, 3, 3)
+Real_GenerateSP_EB = (49, 34, 22, 23, 20, 11, 6, 2, 1, 0)
+Real_GenerateSP_HW = (40, 26, 22, 21, 17, 14, 6, 1, 0, 0)
+Real_GenerateSP_SB = (59, 33, 18, 19, 17, 13, 10, 6, 2, 0)
+
+GenerateSP_EB = (125,0,0,0,0,0,0,0,0,0)
+GenerateSP_HW = (125,0,0,0,0,0,0,0,0,0)
+GenerateSP_SB = (125,0,0,0,0,0,0,0,0,0)

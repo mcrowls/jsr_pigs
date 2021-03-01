18656be108346ee8e9eef6b4b87cf88e36fcbf0f
mcrowls <>
2021-03-01 09:31:23 +0000

    fixing it i fucked up and removed depvar

---
+++ DepVar.py
@@ -0,0 +1,12 @@
+# This file is for variables to do with calculations in the simulation
+
+# Variable used to calculate the pregnancy period for each sow
+PregPeriodMean = 114.605
+PregPeriodSD = 1.572
+
+# Different growth rates for each model
+growth_rate_logistic_model_1 = [0.018460179351268462, 9.078204438994627e-08]
+growth_rate_logistic_model_2 = [0.02387738773877388, 2.910790462317891e-06]
+growth_rate_gompertz_model_1 = [0.0209020902090209, 1.1335600340045393e-07]
+growth_rate_gompertz_model_2 = [0.026602660266026604, 3.3640061009134556e-06]
+growth_rate_linear_model = [0.060555555555, 0.04534777]
